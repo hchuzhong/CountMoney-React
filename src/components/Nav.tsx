5467f6e49b66ea2a669a9cf39f4a7c00e7159b50
chuzhong <352995096@qq.com>
2020-06-27 17:48:34 +0800

    添加 svg 文件

---
+++ src/components/Nav.tsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import React from 'react';
+import x from 'icons/money.svg'
+
+console.log(x);
+
+const NavWrapper = styled.nav`
+  line-height: 24px;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+  > ul{
+    display: flex;
+    >li{
+      width: 33.333%;
+      text-align: center;
+      padding: 16px;
+    }
+  }
+`;
+
+const Nav = () => {
+  return (
+    <NavWrapper>
+      <ul>
+        <li>
+          <Link to="/tags">标签页</Link>
+        </li>
+        <li>
+          <img src={x} alt=""/>
+          <Link to="/money">记账页</Link>
+        </li>
+        <li>
+          <Link to="/statistics">统计页</Link>
+        </li>
+      </ul>
+    </NavWrapper>
+  )
+}
+
+export default Nav;
