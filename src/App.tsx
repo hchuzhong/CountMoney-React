40b88b34afb3bdbd6b09873f0d77f4498434ead9
chuzhong <352995096@qq.com>
2020-06-27 22:46:12 +0800

    将导航栏拆分成组件放到 views 中

--- src/App.tsx
+++ src/App.tsx
@@ -5,60 +5,33 @@ import {
   Route,
   Redirect
 } from 'react-router-dom';
-import styled from 'styled-components';
-import Nav from './components/Nav';
+import Money from './views/Money';
+import Statistics from './views/Statistics';
+import Tags from './views/Tags';
+import NotFound from './views/Notfound';
 
-const Wrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-`;
-const Main = styled.div`
-  flex-grow: 1;
-  overflow: auto;
-`;
 
 function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
-          <Switch>
-            <Route path="/tags">
-              <Tags/>
-            </Route>
-            <Route path="/money">
-              <Money/>
-            </Route>
-            <Route path="/statistics">
-              <Statistics/>
-            </Route>
-            <Redirect exact from="/" to="/money"/>
-            <Route path="*">
-              <NoMatch/>
-            </Route>
-          </Switch>
-        </Main>
-        <Nav/>
-      </Wrapper>
+      <Switch>
+        <Route path="/tags">
+          <Tags/>
+        </Route>
+        <Route path="/money">
+          <Money/>
+        </Route>
+        <Route path="/statistics">
+          <Statistics/>
+        </Route>
+        <Redirect exact from="/" to="/money"/>
+        <Route path="*">
+          <NotFound/>
+        </Route>
+      </Switch>
     </Router>
   );
 }
 
-function Tags() {
-  return <h2>这里是标签页面</h2>;
-}
-
-function Money() {
-  return <h2>这里是记账页面</h2>;
-}
-
-function Statistics() {
-  return <h2>这里是统计页面</h2>;
-}
-
-function NoMatch() {
-  return <h2>页面不存在</h2>;
-}
 
 export default App;
---
+++ src/views/Notfound.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NotFound() {
+  return <h2>页面不存在</h2>;
+}
+
+export default NotFound;
