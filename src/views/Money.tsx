c645d87ecfaa2cd18df41ae1b25fa481990e8b68
chuzhong <352995096@qq.com>
2020-06-29 11:14:35 +0800

    完成记账页面四个部分的 UI，并将其模块化

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,79 +1,20 @@
 import Layout from '../components/Layout';
 import React from 'react';
 import styled from 'styled-components';
+import {CategorySection} from './Money/CategorySection';
+import {TagSection} from './Money/TagSection';
+import {NotesSection} from './Money/NotesSection';
+import {NumberSection} from './Money/NumberSection';
 
-const TagSection = styled.section`
-  background: #ffffff;
-  padding: 12px 16px;
-  >ol{
-  margin: 0 -12px;
-    >li{
-      background: #d9d9d9;
-      border-radius: 18px;
-      display: inline-block;
-      padding: 3px 18px;
-      font-size: 14px;
-      margin: 8px 12px;
-    }
-  }
-  >button{
-    background: none;
-    border: none;
-    padding: 0 4px;
-    border-bottom: 1px solid #333;
-    color: #666;
-    margin-top: 8px;
-  }
-`
-const NotesSection = styled.section`
-  background: #f5f5f5;
-  padding: 0 16px;
-  font-size: 14px;
-  >label{
-    display: flex;
-    align-items: center;
-    >span{
-      margin-right: 16px;
-      white-space: nowrap;
-    }
-    >input{
-      display: block;
-      width: 100%;
-      height: 72px;
-      background: none;
-      border: none;
-    }
-  }
-`
-const CategorySection = styled.section`
-  font-size: 24px;
-  >ul{
-    display: flex;
-    background: #c4c4c4;
-    >li{
-      width: 50%;
-      text-align: center;
-      padding: 16px 0;
-      position: relative;
-      &.selected::after{
-        content: '';
-        display: block;
-        height: 3px;
-        background: #333;
-        position: absolute;
-        bottom: 0;
-        width: 100%;
-        left: 0;
-      }
-    }
-  }
-`
-const NumberSection = styled.section``
-
+const MyLayout = styled(Layout)`
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+`;
 
 function Money() {
   return (
-    <Layout>
+    <MyLayout>
       <TagSection>
         <ol>
           <li>衣</li>
@@ -96,23 +37,25 @@ function Money() {
         </ul>
       </CategorySection>
       <NumberSection>
-        <div>100</div>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>删除</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>清空</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>OK</button>
-        <button>0</button>
-        <button>.</button>
+        <div className="output">100</div>
+        <div className="numberPad clearfix">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>删除</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>清空</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button className="ok">OK</button>
+          <button className="zero">0</button>
+          <button>.</button>
+        </div>
       </NumberSection>
-    </Layout>
+    </MyLayout>
   );
 }
 
