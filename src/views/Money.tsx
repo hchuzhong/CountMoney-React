927a28550f2b27c7c39ea7c2d9c4482612c8a261
chuzhong <352995096@qq.com>
2020-07-04 23:30:33 +0800

    完成备注功能

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import styled from 'styled-components';
 import {CategorySection} from './Money/CategorySection';
 import {TagsSection} from './Money/TagsSection';
-import {NotesSection} from './Money/NotesSection';
+import {NoteSection} from './Money/NoteSection';
 import {NumberSection} from './Money/NumberSection';
 
 const MyLayout = styled(Layout)`
@@ -14,14 +14,8 @@ const MyLayout = styled(Layout)`
 function Money() {
   return (
     <MyLayout>
-      <TagsSection>
-      </TagsSection>
-      <NotesSection>
-        <label>
-          <span>备注</span>
-          <input type="text" placeholder="在这里添加备注"/>
-        </label>
-      </NotesSection>
+      <TagsSection/>
+      <NoteSection/>
       <CategorySection>
         <ul>
           <li className="selected">支出</li>
