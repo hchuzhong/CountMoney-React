b20d9d0d1abe4e226aced2801f66dee573caef61
chuzhong <352995096@qq.com>
2020-07-05 23:33:58 +0800

    完成 NumberPad 功能，并进行了部分优化

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import {CategorySection} from './Money/CategorySection';
 import {TagsSection} from './Money/TagsSection';
 import {NoteSection} from './Money/NoteSection';
-import {NumberSection} from './Money/NumberSection';
+import {NumberPadSection} from './Money/NumberSection';
 
 const MyLayout = styled(Layout)`
   display: flex;
@@ -17,25 +17,7 @@ function Money() {
       <TagsSection/>
       <NoteSection/>
       <CategorySection/>
-      <NumberSection>
-        <div className="output">100</div>
-        <div className="numberPad clearfix">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>删除</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>清空</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button className="ok">OK</button>
-          <button className="zero">0</button>
-          <button>.</button>
-        </div>
-      </NumberSection>
+      <NumberPadSection/>
     </MyLayout>
   );
 }
