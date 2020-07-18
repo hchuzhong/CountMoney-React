92782d2b5fb7026ed646b61d77c8eff6d1aace9a
chuzhong <352995096@qq.com>
2020-07-18 17:08:57 +0800

    创建单个标签的路由

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {useTags} from '../useTags';
 import styled from 'styled-components';
 import Icon from '../components/Icon';
+import {Link} from 'react-router-dom';
 
 const TagList = styled.ol`
   font-size: 16px;
@@ -10,11 +11,13 @@ const TagList = styled.ol`
   > li{
     border-bottom: 1px solid #d5d5d9;
     line-height: 20px;
-    padding: 12px 16px 12px 0;
     margin-left: 16px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+    > a{
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 12px 16px 12px 0;
+    }
   }
 `
 const Button = styled.button`
@@ -42,8 +45,10 @@ function Tags() {
       <TagList>
         {tags.map(tag =>
           <li key={tag}>
-            <span  className="oneLine">{tag}我是一个很长很长很长很长很很长很长很长很长很很长很长很长很长很长很长的句子</span>
-            <Icon name="right"/>
+            <Link to={'/tags/' + tag}>
+              <span  className="oneLine">{tag}</span>
+              <Icon name="right"/>
+            </Link>
           </li>
         )}
       </TagList>
