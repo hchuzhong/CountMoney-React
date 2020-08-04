afd6e41f46f5174f83ef94832341ec4b384a8be7
chuzhong <352995096@qq.com>
2020-08-04 08:52:22 +0800

    实现 Tags 数据持久化

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -39,14 +39,9 @@ type Props = {
   onChange: (selected: number[]) => void
 }
 const TagsSection: React.FC<Props> = (props) => {
-  const {tags, setTags} = useTags()
+  const {tags, addTag} = useTags()
   const selectedTagIds = props.value
-  const onAddTag = () => {
-    const tagName = window.prompt('新标签的名称为');
-    if (tagName !== null) {
-      setTags([...tags, {id: Math.random(), name: tagName}]);
-    }
-  };
+
   const onToggleTag = (tagId: number) => {
     const index = selectedTagIds.indexOf(tagId);
     if (index >= 0) {
@@ -66,7 +61,7 @@ const TagsSection: React.FC<Props> = (props) => {
           >{tag.name}</li>
         )}
       </ol>
-      <button onClick={onAddTag}>新增标签</button>
+      <button onClick={addTag}>新增标签</button>
     </Wrapper>
   );
 };
