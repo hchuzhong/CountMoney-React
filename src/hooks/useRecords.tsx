f2f1641fbf0bb64fcf8829acb25aeef2de54c71e
chuzhong <352995096@qq.com>
2020-08-08 15:19:24 +0800

    重构 useUodate 以消除警告

--- src/hooks/useRecords.tsx
+++ src/hooks/useRecords.tsx
@@ -1,7 +1,7 @@
 import {useEffect, useState} from 'react';
 import {useUpdate} from './useUpdate';
 
-type RecordItem = {
+export type RecordItem = {
   tagIds: number[],
   note: string,
   category: '+' | '-',
@@ -18,7 +18,7 @@ const useRecords = () => {
   }, []);
   useUpdate(() => {
     window.localStorage.setItem('records', JSON.stringify(records));
-  }, [records]);
+  }, records);
 
   const addRecord = (newRecord: newRecordItem) => {
     if (newRecord.amount <= 0) {
